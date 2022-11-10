36fd8ddf68f83f69f9112e138c37985eb816a324
moreno-m2698 <moreno.mw2698@gmail.com>
2022-11-10 13:49:33 -0600

    Reformatting of the file structure and starting implemenation of 'special moves', items, and different units

---
+++ Classes/actions.py
@@ -0,0 +1,33 @@
+class Action:
+    def __init__(self, name, description, id):
+        
+        self.name = name
+        self.id = id
+        self.description = description
+        
+
+
+class Items(Action):
+    def __init__(self, name, description, id, baseChance):
+        super().__init__(self, name, description, id)
+        self.baseChance = baseChance
+        self.amount = 3
+        
+    def itemDrop(self, hero, monster):
+        if monster.epic:
+            if random.randint(1,self.baseChance) == 1:
+                hero.findItem(1)
+
+
+        else:
+            hero.findItem(3)    
+        
+        
+class Specials(Action):
+    def __init__(self, name, description, cost, type, damage, healing):
+        super().__init__(self, name, description, id)
+        self.cost = cost
+        self.type = type
+        self.damage = damage
+        self.healing = healing
+        
